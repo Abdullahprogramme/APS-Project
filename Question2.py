2a8c765fb0f1fa5bd3651c3ea29767b24ad07dde
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2023-11-06 17:39:42 +0500

    Create Question2.py

---
+++ Question2.py
@@ -0,0 +1,20 @@
+def Palindrome(N):
+    if len(N) <= 1: # base case
+        return True
+    elif N[0] == N[-1]: # checks if first and last characters are same or not
+        return Palindrome(N[1:-1]) # recursive case
+    else: return False # base case
+
+def Is_Length_Even(N):
+    return False if (len(N) / 2 == 0) else True # returns True if the length is odd and returns False if it's even
+
+def Cap_Or_NoCap(N):
+    for i in N: # iterates in the string
+        if ord(i) < 65 or ord(i) > 90: return False # checks if the character is not capital letter so returns False
+    return True # if above statement fails it returns True
+
+def Question(N):
+    if (Palindrome(N)) and (Is_Length_Even(N)) and (Cap_Or_NoCap(N)) is True: return "You word is correct" 
+    else: return "Your word is wrong"
+    
+print(Question("RACECAR"))
