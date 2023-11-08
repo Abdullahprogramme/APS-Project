5ec53ec47f14ee1879c1d35669cdddc06309d425
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2023-11-08 22:00:53 +0500

    Create Question3.py

---
+++ Question3.py
@@ -0,0 +1,13 @@
+def Question3():
+    N = int(input("Enter a number you chose: "))
+    number = input("Enter in format: xx yy zz and so on: " )
+    numbers = list(number.split())
+    new = []
+    count = 2
+    for i in range(2, N): # loop through 2 to N
+        for j in range(2, i): # loop through 2 to that number in first loop
+            if i % j == 0: count += 1
+        if count <= 2: new.append(str(i)) # append if prime
+        count = 2 # reset count
+    if new == numbers: print("You are correct")
+    else: print("You are incorrect")
