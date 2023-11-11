c229b13659130ef45b2bb70876f00a176d2bec97
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2023-11-11 22:32:48 +0500

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,5 +1,10 @@
 import numpy as np
-
+def matrix(matrix_a, matrix_b):
+    for index_a, i in enumerate(matrix_a):
+        for index_b, j in enumerate(matrix_b):
+            new_matrix[index_a][index_b] = sum(i * j)
+    return new_matrix
+    
 def enumerated_matrix():
   matrix_a = np.array([[1, 2, 3],
                     [4, 5, 6],
@@ -15,12 +20,7 @@ def enumerated_matrix():
                         [0, 0, 0]])
   useranswer = input("Enter your answer here in the form xx yy zz.....: ") # user input
   a1, a2, a3, b1, b2, b3, c1, c2, c3 = useranswer.split(" ") 
-  def matrix(matrix_a, matrix_b):
-    for index_a, i in enumerate(matrix_a):
-        for index_b, j in enumerate(matrix_b):
-            new_matrix[index_a][index_b] = sum(i * j)
-    return new_matrix
-    
+  
   answer = matrix(matrix_a, matrix_b)
   if answer[0] == [int(a1), int(a2), int(a3)] and answer[1] == [int(b1), int(b2), int(b3)] and answer[2] == [int(c1), int(c2), int(c3)]:
     print("You are correct")
