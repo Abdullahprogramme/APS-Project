14f30a4d1fdac651c87735d33a43f8460d4af7bb
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2023-11-14 12:17:07 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-print(" ")
+asas = {1: Question}
+tries = 1
+flag = True
+while tries <= 4 and flag is True:
+    if tries != 4:
+        tries, answer = asas[1]()
+    if tries < 4:
+        print(answer)
+    elif tries == 4 and flag is True: print("You failed")
+    tries += 1
