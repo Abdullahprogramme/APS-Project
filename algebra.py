3897ff1ad9aec1bcbfa373c50a1af5381a1b0dfa
Ahla Haider <150292602+AhlaHaider@users.noreply.github.com>
2023-11-29 14:57:09 +0500

    Update and rename countwords to algebra.py

---
+++ algebra.py
@@ -0,0 +1,18 @@
+import random
+
+def algebraic_question():
+    # Generate random values for the equation ax + b = c
+    a = random.randint(2, 5)
+    b = random.randint(1, 10)
+    c = a * random.randint(6, 10) + b
+
+    question = f"Solve for x: {a}x + {b} = {c}"
+    answer = (c - b) / a
+
+    return question, answer
+# Generate question and its answer
+question, answer = algebraic_question()
+# Display the question
+#print(question)
+# print(f"Correct Answer: {answer}")
+
