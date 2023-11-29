f9f1c87e2502a082c6bb417332184ded3964b19c
Ahla Haider <150292602+AhlaHaider@users.noreply.github.com>
2023-11-29 15:26:06 +0500

    Update algebra.py

--- algebra.py
+++ algebra.py
@@ -3,13 +3,14 @@ import random
 def algebraic_question():
     # Generate random values for the equation ax + b = c
     a = random.randint(2, 5)
-    b = random.randint(1, 10)
-    c = a * random.randint(6, 10) + b
+    b = random.randint(1, 20)
+    x = random.randint(6, 10)
+    c = a * x + b
 
     question = f"Solve for x: {a}x + {b} = {c}"
     answer = (c - b) / a
-
     return question, answer
+    
 # Generate question and its answer
 question, answer = algebraic_question()
 # Display the question
