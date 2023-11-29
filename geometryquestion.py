046f2813d7154568b64af56edb2ee5b4234a3c64
Ahla Haider <150292602+AhlaHaider@users.noreply.github.com>
2023-11-29 15:34:46 +0500

    Update geometryquestion.py

--- geometryquestion.py
+++ geometryquestion.py
@@ -1,16 +1,22 @@
 import random
 
-def generate_geometry_question():
+def geometry_question():
     base = random.randint(5, 15)
     height = random.randint(3, 10)
     question = f"Find the area of a triangle with base {base} units and height {height} units."
 
     # Calculate area of the triangle
     correct_answer = 0.5 * base * height
+    # Display the question
+    print(question)
     return question, correct_answer
 
 # Generate question and its correct answer
-question, correct_answer = generate_geometry_question()
-# Display the question
-#print(question)
-# print(f"Correct Answer: {correct_answer}")
+question, correct_answer = geometry_question()
+
+#checking users answers
+user_answer = float(input("Enter your answer: "))
+if user_answer == correct_answer:
+    print("Congratulations! Your answer is correct.")
+else:
+    #print(f"Sorry, the correct answer is {correct_answer}.")
